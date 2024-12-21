5f2966e0a4366022eafcdaf58b9a4cf7fc9f40c0
minnyres <liuthgw23@gmail.com>
2024-12-21 18:33:53 +0800

    update

--- build.ps1
+++ build.ps1
@@ -6,6 +6,7 @@ $env:path = "$vcpkg_dir\installed\x64-windows-release\bin;$qt_dir\bin;$fop_dir;$
 # sed -i 's|!defined(MIKTEX_WINDOWS)|defined(MIKTEX_WINDOWS)|g' miktex\Libraries\3rd\fontconfig\source\src\fccompat.c
 mv "$vcpkg_dir\installed\x64-windows-release\include\dirent.h" "$vcpkg_dir\installed\x64-windows-release\include\dirent.h.copy"
 mv "$vcpkg_dir\installed\x64-windows-release\include\getopt.h" "$vcpkg_dir\installed\x64-windows-release\include\getopt.h.copy"
+sed -i "s|typedef unsigned long  uLong; \/\* 32 bits or more \*\/|typedef unsigned long  uLong; \/\* 32 bits or more \*\/ \n #  define FAR|g" "$vcpkg_dir\installed\x64-windows-release\include\zconf.h"
 
 cd miktex
 git apply ..\fix-use-system-fontconfig.patch
@@ -14,7 +15,7 @@ cd ..
 cmake -B "build-x64" -G "Ninja" -DCMAKE_BUILD_TYPE=Release `
  -DCMAKE_INSTALL_PREFIX=$miktex_install -DWITH_MIKTEX_API_DOC=OFF `
  -DCMAKE_TOOLCHAIN_FILE="$vcpkg_dir\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-release `
- -DUSE_SYSTEM_BZIP2=ON -DUSE_SYSTEM_CAIRO=ON -DUSE_SYSTEM_EXPAT=ON -DUSE_SYSTEM_FONTCONFIG=ON -DUSE_SYSTEM_FREETYPE2=ON `
+ -DUSE_SYSTEM_BZIP2=ON -DUSE_SYSTEM_CAIRO=ON -DUSE_SYSTEM_EXPAT=ON -DUSE_SYSTEM_FONTCONFIG=ON -DUSE_SYSTEM_FREETYPE2=ON -DUSE_SYSTEM_ICU=ON `
  -DUSE_SYSTEM_PIXMAN=ON -DUSE_SYSTEM_PNG=ON -DUSE_SYSTEM_ZLIB=ON -DUSE_SYSTEM_APR=ON -DUSE_SYSTEM_APRUTIL=ON -DUSE_SYSTEM_OPENSSL_CRYPTO=ON `
  -DCMAKE_CXX_FLAGS="/W0 /DWIN32" -DCMAKE_C_FLAGS="/W0 /DWIN32" miktex
 cmake --build build-x64
